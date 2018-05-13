b646b1e09e2487d089ababead1bc4c12dc15164f
Pearu Peterson <pearu.peterson@gmail.com>
2018-05-13 17:33:40 +0300

    Add new example kernel add_one.

--- xndlib/examplelib/example.c
+++ xndlib/examplelib/example.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include "example.h"
 
+void i_add_one(int x, int* r) { *r = x + 1; }
+void l_add_one(long x, long* r) { *r = x + 1; }
+void d_add_one(double x, double* r) { *r = x + 1.0; }
+void s_add_one(float x, float* r) { *r = x + 1.0; }
+
 void d_example_sum(long n, double* x, double* r) {
   long i;
   double s = 0.0;
   for (i=0;i<n;i++)
     s += x[i];
-  printf("d_example_sum: n = %d, s = %f\n", n, s);
   *r = s;
 }
 
--- xndlib/examplelib/example.h
+++ xndlib/examplelib/example.h
@@ -1,4 +1,9 @@
 
+extern void i_add_one(int x, int* r);
+extern void l_add_one(long x, long* r);
+extern void d_add_one(double x, double* r);
+extern void s_add_one(float x, float* r);
+
 extern void d_example_sum(long n, double* x, double* r);
 extern void s_example_sum(long n, float* x, float* r);
 extern void i_example_sum(long n, int* x, int* r);
