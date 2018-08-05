82e93e37f29b67c432ff990164ce679551751fc6
Pearu Peterson <pearu.peterson@gmail.com>
2018-08-05 18:23:21 +0300

    Better script to update xnd deps.

--- update_xnd.sh
+++ update_xnd.sh
@@ -5,24 +5,37 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # assuming conda environment:
 export PREFIX=$CONDA_PREFIX
 
-cd $DIR/../ndtypes
+if ! [ -d $DIR/ndtypes ]; then
+  git clone git@github.com:plures/ndtypes.git
+fi
+
+if ! [ -d $DIR/xnd ]; then
+  git clone git@github.com:plures/xnd.git
+fi
+
+if ! [ -d $DIR/gumath ]; then
+  git clone git@github.com:plures/gumath.git
+fi
+
+cd $DIR/ndtypes
 git pull
 ./configure --prefix=$PREFIX
 make
 make install
 pip install -U .
 
-cd $DIR/../xnd
+cd $DIR/xnd
 git pull
 ./configure --prefix=$PREFIX --with-includes=$PREFIX/include
 make
 make install
 pip install -U .
 
-cd $DIR/../gumath
+cd $DIR/gumath
 git pull
 ./configure --prefix=$PREFIX --with-includes=$PREFIX/include
 make
 make install
 pip install -U .
+
 cd $CWD
