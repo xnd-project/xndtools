aca117a8342cdb18f9bf065d94f7baad4b7e79ca
Pearu Peterson <pearu.peterson@gmail.com>
2018-08-20 00:07:07 +0300

    Move test_mixed to xndtools.

--- xndtools/kernel_generator/tests/test_mixed.c
+++ xndtools/kernel_generator/tests/test_mixed.c
@@ -1,4 +1,9 @@
 long test_mixed_matrices(long n, long* a, long* b)
 {
+  /* a[0,n-1] - C layout
+     a[n-1,0] - F layout 
+     b[n-1,0] - C layout
+     b[0,n-1] - F layout
+   */
   return a[n-1] + b[(n-1)*n];
 }
--- xndtools/kernel_generator/tests/test_test_mixed.py
+++ xndtools/kernel_generator/tests/test_test_mixed.py
@@ -1,7 +1,13 @@
 import pytest
 from xnd import xnd
-#import test_mixed as m
 
+try:
+    import test_mixed as m
+    skip_reason = None
+except Exception as msg:
+    m = None
+    skip_reason = f'Failed to import test_mixed: {msg}'
+    
 def unpack(x):
     if isinstance(x, xnd):
         return str(x.type), x.value
@@ -12,9 +18,39 @@ def unpack(x):
 def assert_equal(x, y):
     assert unpack(x) == unpack(y)
 
-@pytest.mark.skip(reason="ndtypes does not support mixed arguments??, importing test_mixed failes")
-def test_mixed_matrices_input():
-    a = xnd([[10,20], [30,40]], dtype='2 * 2 * int64')
-    b = xnd([[5,6], [7,8]], dtype='!2 * 2 * int64')
-    r = m.test_mixed_matrices_input(a, b)
+@pytest.mark.skipif(m is None or not hasattr(m, 'test_mixed_matrices_inout_CF'),
+                    reason=skip_reason or 'test_mixed_matrices_inout_CF not supported by ndtypes')
+def test_mixed_matrices_CF_inout():
+    a = xnd([[10,20],
+             [30,40]], type='2 * 2 * int64')
+    b = xnd([[5,6],
+             [7,8]], type='!2 * 2 * int64')
+    r = m.test_mixed_matrices_inout_CF(a, b)
     assert_equal(r, xnd(26))
+
+@pytest.mark.skipif(m is None, reason=skip_reason)
+def test_mixed_matrices_CC_inout():
+    a = xnd([[10,20],
+             [30,40]], type='2 * 2 * int64')
+    b = xnd([[5,6],
+             [7,8]], type='2 * 2 * int64')
+    r = m.test_mixed_matrices_inout_CC(a, b)
+    assert_equal(r, xnd(27))
+    return
+    a = xnd([[10,20],
+             [30,40]], type='!2 * 2 * int64')
+    b = xnd([[5,6],
+             [7,8]], type='!2 * 2 * int64')
+    r = m.test_mixed_matrices_inout_CC(a, b)
+    assert_equal(r, xnd(27))
+
+    
+@pytest.mark.skipif(m is None or not hasattr(m, 'test_mixed_matrices_inout_FF'),
+                    reason=skip_reason or 'test_mixed_matrices_inout_FF not supported by ndtypes')
+def test_mixed_matrices_FF_inout():
+    a = xnd([[10,20],
+             [30,40]], type='!2 * 2 * int64')
+    b = xnd([[5,6],
+             [7,8]], type='!2 * 2 * int64')
+    r = m.test_mixed_matrices_inout_FF(a, b)
+    assert_equal(r, xnd(36))
